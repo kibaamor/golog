89b81ea0bc6912f3fc68d1f0f6d09f5284213fa2
kibaamor <5093911+kibaamor@users.noreply.github.com>
2021-12-13 19:01:08 +0800

    fix caller path issues

--- decorate.go
+++ decorate.go
@@ -94,7 +94,7 @@ func HandlerTimestamp(keyName, valueFormat string, nowFunc func() time.Time) Han
 func HandlerCaller(keyName string, depth int, withFullpath bool) Handler {
 	return func(ctx context.Context, level Level, kvs []interface{}) []interface{} {
 		_, file, line, _ := runtime.Caller(depth)
-		for strings.LastIndex(file, "/log/helper.go") > 0 {
+		for strings.LastIndex(file, "/golog/helper.go") > 0 {
 			depth++
 			_, file, line, _ = runtime.Caller(depth)
 		}
